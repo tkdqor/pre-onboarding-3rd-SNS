7031367bece26ec70f6c3b212cba13328f33775e
sangbaek <aumsbk@naver.com>
2022-07-20 21:42:52 +0900

    Feat : #3 token 재발급 기능 추가 - refresh token과 함께 POST 요청 시, access token과 refresh token 모두 재발급

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views import SignInView, SignUpView
 
@@ -7,4 +8,5 @@ app_name = "accounts"
 urlpatterns = [
     path("v1/users/signup", SignUpView.as_view()),
     path("v1/users/signin", SignInView.as_view()),
+    path("v1/token/refresh", TokenRefreshView.as_view()),
 ]
